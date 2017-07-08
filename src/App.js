d238fac7948b0ea264d3ae8547de58e1d053489f
davidkim310 <davidkim310@gmail.com>
2017-07-08 12:10:32 -0700

    adds templating

--- src/App.js
+++ src/App.js
@@ -4,9 +4,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div className="App container">
+        <br></br>
+        <div className="row">
+          <div className="col-md-12">
+            EATERY
+          </div>
+          <div className="col-md-12">
+            <button className="btn btn-primary">?</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            Places To Eat
+            <div className="form-group">
+              <input type="text" className="form-control" id="" placeholder="Name"></input>
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="" placeholder="Name"></input>
+            </div>
+            <button className="btn btn-success">+</button>
+          </div>
+          <div className="col-sm-6">
+            Places Eaten
+            <div className="form-group">
+              <input type="text" className="form-control" id="" placeholder="Name"></input>
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="" placeholder="Name"></input>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <button className="btn btn-danger">Reset</button>
         </div>
       </div>
     );
